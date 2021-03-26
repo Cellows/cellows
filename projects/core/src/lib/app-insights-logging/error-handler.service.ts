3b4ec161f53deea005eec528e9a0d8ede55ad7c7
Jef <jwachters@users.noreply.github.com>
2021-03-26 16:35:54 +0100

    Add compodoc and code documentation (#11)

--- projects/core/src/lib/app-insights-logging/error-handler.service.ts
+++ projects/core/src/lib/app-insights-logging/error-handler.service.ts
@@ -1,14 +1,25 @@
 import { ErrorHandler, Injectable } from "@angular/core";
 import { AppInsightsLoggingService } from "./app-insights-logging.service";
 
+/**
+ * Error handler service which logs exceptions to Azure Application Insights
+ */
 @Injectable()
 export class ErrorHandlerService extends ErrorHandler {
 
+  /**
+   * Error handlers service constructor
+   * @param loggingService Application Insights logging service
+   */
   constructor(private loggingService: AppInsightsLoggingService) {
     super();
   }
 
+  /**
+   * Manually log an exception to Application Insights
+   * @param error Exception which is thrown
+   */
   handleError(error: Error) {
-    this.loggingService.logException(error); // Manually log exception
+    this.loggingService.logException(error);
   }
 }
